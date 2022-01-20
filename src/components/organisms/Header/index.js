4ec9e2352a006e596c5ff3387e27a8210327ce56
José Barros <barros@rede.ulbra.br>
2022-01-20 17:59:23 -0300

    FIX: [FRONT] Correção do componente de foto do usuário

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -5,13 +5,15 @@ import MainNav from '../../molecules/MainNav'
 import ProfileInfo from '../../molecules/ProfileInfo'
 
 const Header = () => {
+    const photo = JSON.parse(localStorage.getItem('@UbiRH/user_photo'))
+
     return (
         <HeaderHome>
             <LogoUbistart width="120px" margin="0 0 0 4em"/>
             <ContainerNav>
                 <MainNav/>
             </ContainerNav>
-            <ProfileInfo/>
+            <ProfileInfo photo={photo}/>
         </HeaderHome>
     )
 }
