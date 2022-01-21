c59a0c7864992f10318f1e3cf42a063900ddbbab
José Barros <barros@rede.ulbra.br>
2022-01-21 00:54:57 -0300

    WIP: [FRONT] Configuração das rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,72 +1,73 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 
-import PrivateRoute from './PrivateRoute';
-import Login from "../components/pages/Login";
-import Home from "../components/pages/Home";
-import Professionals from "../components/pages/Professionals";
+import PrivateRoute from './PrivateRoute'
+import Login from "../components/pages/Login"
+import Home from "../components/pages/Home"
+import Professionals from "../components/pages/Professionals"
 import Projects from "../components/pages/Projects"
 import Overtime from '../components/pages/Overtime'
 import Invoice from '../components/pages/Invoice'
 import Reports from '../components/pages/Reports'
 import ServiceOrders from '../components/pages/ServiceOrders'
-import Settings from '../../src/components/pages/Settings';
-import Status from '../components/pages/Status';
-import ProjectType from '../components/pages/ProjectType';
+import Settings from '../../src/components/pages/Settings'
+import Status from '../components/pages/Status'
+import ProjectType from '../components/pages/ProjectType'
 import RegisterProject from '../components/pages/RegisterProject'
-import RegisterProfessional from '../components/pages/RegisterProfessional';
+import RegisterProfessional from '../components/pages/RegisterProfessional'
+import TimeSending from '../components/pages/TimeSending'
+import InvoiceSending from '../components/pages/InvoiceSending/InvoiceSending'
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Login}/>
+                <PrivateRoute path="/home" component={Home} pageTitle="Início"/>
+                <PrivateRoute 
+                    exact path="/professionals" 
+                    component={Professionals} 
+                    pageTitle="Profissionais"
+                    button="Cadastrar novo"
+                    buttonPath="/professional"
+                 />
+                <PrivateRoute 
+                    exact path="/projects" 
+                    component={Projects} 
+                    pageTitle="Projetos"
+                    button="Cadastrar novo"
+                    buttonPath="/project"
+                />
+                <PrivateRoute path="/overtime" component={Overtime} pageTitle="Horas extras" />
+                <PrivateRoute path="/invoice" component={Invoice} pageTitle="Notas fiscais" />
+                <PrivateRoute path="/reports" component={Reports} pageTitle="Relatórios" />
+                <PrivateRoute path="/serviceOrders" component={ServiceOrders} pageTitle="Ordens de serviço" />
+                <PrivateRoute path="/job" component={Settings} pageTitle="Configurações" />
+                <PrivateRoute path="/projectStatus" component={Status} pageTitle="Configurações" />
+                <PrivateRoute path="/projectType" component={ProjectType} pageTitle="Configurações" />
+                <PrivateRoute exact path="/project" component={RegisterProject} registerPage />
+                <PrivateRoute path="/project/:id" component={RegisterProject} registerPage />
+                <PrivateRoute exact path="/professional" component={RegisterProfessional} registerPage />
+                <PrivateRoute path="/professional/:id" component={RegisterProfessional} registerPage />
+
+                {/* Profissional */}
+                <PrivateRoute 
+                    path="/timeSending" 
+                    component={TimeSending} 
+                    pageTitle="Lançamento de horas"
+                    button="Novo lançamento"
+                    buttonPath="/"
+                />
+                <PrivateRoute 
+                    path="/InvoiceSending" 
+                    component={InvoiceSending} 
+                    pageTitle="Notas fiscais"
+                    button="Enviar NF"
+                    buttonPath="/"
+                />
             </Switch>
-            <Switch>
-                <PrivateRoute path="/home" component={Home}/>
-            </Switch>
-            <Switch>
-                <Route exact path="/professionals" component={Professionals}/>
-            </Switch>
-            <Switch>
-                <Route exact path="/projects" component={Projects}/>
-            </Switch>
-            <Switch>
-                <Route path="/overtime" component={Overtime}/>
-            </Switch>
-            <Switch>
-                <Route path="/invoice" component={Invoice}/>
-            </Switch>
-            <Switch>
-                <Route path="/reports" component={Reports}/>
-            </Switch> 
-             <Switch>
-                <Route path="/serviceOrders" component={ServiceOrders}/>
-            </Switch>
-            <Switch>
-                <Route path="/job" component={Settings}/>
-            </Switch>
-            <Switch>
-                <Route path="/projectStatus" component={Status}/>
-            </Switch>
-            <Switch>
-                <Route path="/projectType" component={ProjectType}/>
-            </Switch>
-            <Switch>
-                <Route exact path="/project" component={RegisterProject}/>
-            </Switch>
-            <Switch>
-                <Route path="/project/:id" component={RegisterProject}/>
-            </Switch>
-            <Switch>
-                <Route exact path="/professional" component={RegisterProfessional}/>
-            </Switch>
-            <Switch>
-                <Route path="/professional/:id" component={RegisterProfessional}/>
-            </Switch>
-            
         </BrowserRouter>
     )
 }
 
-export default Routes;
+export default Routes
