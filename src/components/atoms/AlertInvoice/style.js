3f9d44dcb4311a0395c4fbc264a91bf82f73132c
José Barros <barros@rede.ulbra.br>
2022-01-31 09:58:35 -0300

    WIP: [Front] Criação dos componentes

---
+++ src/components/atoms/AlertInvoice/style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const Main = styled.div`
+  display: flex;
+  padding: 2em 5em;
+`
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  background: #F4F6F8;
+  font-size: 14px;
+  font-style: italic;
+  border-radius: 8px;
+  padding: 2em;
+`
+
+export const Text = styled.p`
+  margin-left: 1em;
+  line-height: 1em;
+`
---
+++ src/components/molecules/TitleContainer/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+export const Main = styled.header`
+    display: flex;
+    padding: 2em 0 1em 10%;
+`
