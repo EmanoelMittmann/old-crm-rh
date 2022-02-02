90e9f3fbedb19ed9e94f0c03129a8e784f22eb3f
José Barros <barros@rede.ulbra.br>
2022-02-02 13:50:17 -0300

    fix: comentário checkArraysDifference

--- src/components/utils/checkArraysDifference.js
+++ src/components/utils/checkArraysDifference.js
@@ -1,4 +1,3 @@
-// primeiro parâmetro é o array que contem todos os itens, o segundo parâmetro é o array com itens para comparação e o terceiro parâmetro é a chave que eu desejo comparar;
 import differenceBy from 'lodash.differenceby'
 
 export function checkArraysDifference({completeArray, comparisonArray, key}) {  
