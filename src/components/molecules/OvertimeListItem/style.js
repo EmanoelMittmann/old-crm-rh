5c3dc3553c3cdeaee9d9e4ce7fc58df4b177cef2
José Barros <61697278+zzzbarros@users.noreply.github.com>
2022-03-15 15:25:17 -0300

    WIP: Add OvertimeList

---
+++ src/components/molecules/OvertimeListItem/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export const Main = styled.div`
+    display: grid;
+    grid-template-columns: .5fr 1fr 2fr 1fr;
+    padding-left: 3.3em;
+    width: 100%;
+    height: 50px;
+    align-items: center;
+`
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+`
+export const StatusContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: 1em;
+`
---
+++ src/components/organisms/OvertimeList/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ListHeader from '../../molecules/ListHeader'
+import { OvertimeListItem } from '../../molecules/OvertimeListItem'
+import Footer from '../Footer'
+
+const headers = [
+  {
+    name: "#",
+    order: true,
+    field: "id"
+  },
+  {
+    name: "Período",
+    order: false,
+  },
+  {
+    name: "Projeto",
+    order: false,
+  },
+  {
+    name: "Status",
+    order: false,
+    endContainer: true
+  }
+]
+
+export function OvertimeList({ data, meta, nextPage, previousPage, sortById, status }) {
+  return (
+    <>
+      <ListHeader 
+        data={headers}
+        fnOrder={sortById}  
+      />
+      <OvertimeListItem data={data} status={status}/>
+      <Footer
+        previousPage={previousPage}
+        nextPage={nextPage}
+        currentPage={meta?.current_page}
+        firstPage={meta?.first_page}
+        lastPage={meta?.last_page}
+      />  
+    </>
+  )
+}
