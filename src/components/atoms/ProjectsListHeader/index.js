9dd592a8a52b917d6659ca86a1f4b5fdee8ed318
José Barros <61697278+zzzbarros@users.noreply.github.com>
2022-04-22 17:30:28 -0300

    wip: refact ProjectsListHeader

--- src/components/atoms/ProjectsListHeader/index.js
+++ src/components/atoms/ProjectsListHeader/index.js
@@ -1,7 +1,4 @@
-import React,{ useState} from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-
-import api from '../../../api/api'
+import React from 'react'
 import {
     ListHeaderProjeto,
     ListHeaderType,
@@ -9,53 +6,21 @@ import {
     ListHeaderTime,
     ListHeaderStatus
 } from './style.js'
-
-import { 
-    setFilterOrderProjects, 
-    projectsPages, 
-    setProjectList 
-}from '../../../redux/actions/index.js'
-
 import { ListHeaderContainer, ListHeaderTitle } from '../ListHeader/style'
 import { ReactComponent as Arrows } from '../../../assets/icons/arrows.svg'
 
-const ProjectsListHeader = () => {
-    const state = useSelector(state => state)
-    const dispatch = useDispatch()
-    const [isAsc, setIsAsc] = useState(true)
+const fields = {
+    NAME: 'name',
+    DATA_START: 'date_start',
+}
 
-
-    const orderProjectsList = async () => {
-      //  const isActive = state.filterStatus
-        const paramsOrder = isAsc ? 'asc' : 'desc'
-
-        setIsAsc(!isAsc)
-
-        const {data} = await api({
-            method:'get',     
-            url:`/project`,
-            params: {
-                page: 1,
-                orderField: 'name',
-                order: paramsOrder,
-                search: state.projectsSearchFilter,
-            }
-        }); 
-
-        dispatch(setFilterOrderProjects(paramsOrder))
-        dispatch(setProjectList(data.data));
-        dispatch(projectsPages(data.meta));
-
-
-        return data;
-    }
+const ProjectsListHeader = ({ fnOrder }) => {
 
     return (
         <ListHeaderContainer>
-
             <ListHeaderProjeto>
-                <ListHeaderTitle onCLick={()=> orderProjectsList()}>Projeto</ListHeaderTitle>
-                <Arrows onClick={() => orderProjectsList()}/>
+                <ListHeaderTitle onClick={() => fnOrder(fields.NAME)}>Projeto</ListHeaderTitle>
+                <Arrows onClick={() => fnOrder(fields.NAME)}/>
             </ListHeaderProjeto>
 
             <ListHeaderType>
@@ -63,8 +28,8 @@ const ProjectsListHeader = () => {
             </ListHeaderType>
 
             <ListHeaderBeginning>
-                <ListHeaderTitle onCLick={()=> orderProjectsList()}>Início</ListHeaderTitle>
-                <Arrows onClick={() => orderProjectsList()}/>
+                <ListHeaderTitle onClick={()=> fnOrder(fields.DATA_START)}>Início</ListHeaderTitle>
+                <Arrows onClick={() => fnOrder(fields.DATA_START)}/>
             </ListHeaderBeginning>
 
             <ListHeaderTime>
@@ -78,4 +43,4 @@ const ProjectsListHeader = () => {
     )
 }
 
-export default ProjectsListHeader;
+export default ProjectsListHeader
