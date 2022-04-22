de1a2bcfde3d8e7c4af3396b0ed4b8868e8e2027
José Barros <61697278+zzzbarros@users.noreply.github.com>
2022-04-22 17:32:51 -0300

    wip: Refact ProjectsListItem e alteração para organismo

--- src/components/organisms/ProjectsListItem/style.js
+++ src/components/organisms/ProjectsListItem/style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const ProjectsListItemContainer = styled.div`
     width: 100%;
@@ -53,40 +53,4 @@ export const ContainerTeamMemberPic = styled.div`
     position: relative;
     display: flex;
     align-items: flex-end;
-`
-
-export const TeamMemberDetails = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    font-family: "Poppins", sans-serif;
-    font-weight: 500;
-    position: absolute;
-    left: ${props => props.position};
-    width: 155px;
-    height: 50px;
-    padding-left: 20px;
-    border-radius: 8px; 
-    background-color: white;
-    box-shadow: 0px 16px 32px #919EAB66;
-    z-index: 5;
-
-    &:before {
-        content: "";
-        width: 20px;
-        height: 25px;
-        left: -10px;
-        top: 13px;
-        clip-path: polygon(0 50%, 100% 100%, 100% 0);
-        position: absolute;
-        background-color: white;
-  }
-`
-
-export const Name = styled.p`
-    font-size: 0.9rem;
-`
-
-export const WorkLoad = styled.span`
-    font-size: 0.7rem;
 `
