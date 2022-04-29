272cc98c56bbe095e4b391058d319ba8aa25dc80
José Barros <61697278+zzzbarros@users.noreply.github.com>
2022-04-29 11:28:32 -0300

    WIP:  Refact attachment/Team

--- src/components/organisms/Attachment/Team/index.js
+++ src/components/organisms/Attachment/Team/index.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import { BsThreeDotsVertical } from "react-icons/bs";
-
+import React, { useState, useEffect, useLayoutEffect } from 'react'
+import { BsThreeDotsVertical } from "react-icons/bs"
+import { useParams } from 'react-router-dom'
 import { checkArraysDifference } from '../../../utils/checkArraysDifference'
-
 import {
     AttachmentContainer,
     AttachmentForm,
@@ -14,265 +13,162 @@ import {
     ProfessionalName,
     ProfessionalJob,
     ProfessionalHours,
+    ProfessionalStatus,
     ProfilePicture,
+    ProfessionalOvertime,
     ProfessionalProfilePicture
 } from './style.js'
-import api from '../../../../api/api'
 import User from '../../../../assets/user.png'
-import { formatFirstLetter } from '../../../utils/formatFirstLetter.js';
 import { BlueButton } from '../../../atoms/Buttons/BlueButton/style.js'
 import SecondaryText from '../../../atoms/SecondaryText/style'
 import InputSelectWithLabel from '../../../atoms/InputSelectWithLabel'
 import InputText from '../../../atoms/InputText'
 import { ListHeaderContainer, ListHeaderTitle } from '../../../atoms/ListHeader/style.js'
+import MenuOptions from '../../../atoms/MenuOptions'
+import { Badge } from '../../../atoms/Badge'
 import ModalRed from '../../../molecules/ModalRed'
-import MenuOptions from '../../../atoms/MenuOptions';
-import ModalEditAttachment from '../../../molecules/ModalEditAttachment';
+import ModalEditAttachment from '../../../molecules/ModalEditAttachment'
 
-const AttachmentTeam = ({projectId, componentRendered, editData, payloadTeam, setPayloadTeam}) => {
+const status = {
+    ATIVO: {
+        name: 'Ativo',
+        color: {
+            button_color: '#E4F8DD',
+            text_color: '#229A16'
+        }
+    },
+    INATIVO: {
+        name: 'Inativo',
+        color: {
+            button_color: '#FFE2E1',
+            text_color: '#BB2B3F'
+        }
+    }
+}
+
+const AttachmentTeam = ({ attachment, allOptions }) => {
+    const { team, setTeam, addMember, removerMember, editMember } = attachment
+    const [rows, setRows] = useState([])
+    const [options, setOptions] = useState([])
+    const [professionalSelected, setProfessionalSelected] = useState(null)
     const [hoursMonth, setHoursMonth] = useState('')
     const [overtime, setOvertime] = useState('')
-    //Todos os possíveis membros do time
-    const [allProfessionals, setAllProfessionals] = useState([])
-    //Membro do time selecionado
-    const [professionalSelected, setProfessionalSelected] = useState(null)
-    //Membros do time
-    const [teamMembers, setTeamMembers] = useState([])
-    //reset Inputs
     const [reset, setReset] = useState(true)
-
-    const [teamMemberDeleteId, setTeamMemberDeleteId] = useState(0)
     const [modalIsVisible, setModalIsVisible] = useState(false)
-
-    const [editTeamRealTime, setEditTeamRealTime] = useState(false)
-    const [deleteRealTime, setDeleteRealTime] = useState(false)
-    const [deleteTeamMemberId, setDeleteTeamMemberId] = useState(false)
-
     const [menuOptionsIsVisible, setMenuOptionsIsVisible] = useState(false)
     const [professionalClicked, setProfessionalClicked] = useState('')
     const [openModalEdit, setOpenModalEdit] = useState(false)
     const [hoursMonthEdit, setHoursMonthEdit] = useState('')
     const [overtimeEdit, setOvertimeEdit] = useState('')
+    const { id } = useParams()
 
+    useLayoutEffect(() => {
+        const optionsValid = checkArraysDifference({
+            completeArray: allOptions,
+            comparisonArray: team, 
+            key: 'id'
+        })
+        setOptions(optionsValid)
+        handleRows()
+    },[allOptions, team])
 
+    function handleRows() {
+        setRows([])
+        team.map(member => {
+          const item = {
+            id: member.id,
+            avatar: member.avatar,
+            name: member.name,
+            job: member.job.name || member.job,
+            status: member.is_active || member.status,
+            workload: member.workload, 
+            extra_hours_limit: member.extra_hours_limit
+          }
+          setRows(oldState => [...oldState, item])
+        })
+    }
 
-    //////////////////////Funções usadas nos dois tipos de edição//////////////////////
-   
-    const isEditing = projectId ? true : false
-    
-    const CloseButtonClickHandler = () => {
+    function handleAddMember() {
+        if(!professionalSelected) return
+        const selected = allOptions.find(member => member.id == professionalSelected)
+
+        if(!id) {
+            setTeam(oldState => [...oldState, {
+                id: selected.id,
+                name: selected.name,
+                workload: hoursMonth,
+                extra_hours_limit: overtime,
+                avatar: selected.avatar,
+                job: selected.job.name,
+                status: selected.is_active,
+            }])
+            resetInputs()
+            return
+        }
+        
+        addMember(professionalSelected, hoursMonth, overtime)
+        resetInputs()
+    }
+
+    function handleEditMember() {
+        if(!id) {
+            const edited = team.map((member) => {
+                if(member.id == professionalClicked){
+                    return {...member, workload: hoursMonthEdit, extra_hours_limit: overtimeEdit}
+                }
+                if(member.id !== professionalClicked){
+                    return member
+                }
+            })
+            setTeam(edited)
+            setOpenModalEdit(false)
+            return
+        }
+        editMember(professionalClicked, hoursMonthEdit, overtimeEdit)
+        setOpenModalEdit(false)
+    }
+
+    function handleRemoveMember() {
+        if(!id) {
+            const data = team.filter((member) => member.id !== professionalClicked)
+            setTeam(data)
+            setModalIsVisible(false)
+            return
+        }
+        removerMember(professionalClicked)
         setModalIsVisible(false)
     }
 
-    const getAllProfessionals = async () => {
-        const {data} = await api({
-            method:'get',     
-            url:`/user`,
-        })
-        const validMembers = checkArraysDifference({
-            completeArray: data, 
-            comparisonArray: teamMembers, 
-            key: "id"
-        })
-        const formattedProfessionals = formatFirstLetter(validMembers)
-
-        return setAllProfessionals(formattedProfessionals)
-    }
-    ////////////////////// Funções usadas na edição a nivel de front-end //////////////////////
-
-    const setTeamMemberClickHandler = () => {
-
-        // o profissional selecionado é vinculado ao time
-    
-        //Informações do profissional selecionado
-        const newTeamMember = allProfessionals.filter((professional) => {
-            return professional.id === professionalSelected
-        })
-        
-        const [{id, name, avatar, job}] = newTeamMember
-        
-        const newTeamMembers = [...teamMembers, {
-            user_id: id,
-            name: name,
-            avatar: avatar,
-            job_name: job?.name,
-            workload: +hoursMonth,
-            extra_hour_limit: +overtime,
-        }];
-        
-            //Validação para se o membro do time já existe
-             const memberAlreadyExist = teamMembers.find((member) => {
-                return member.user_id === id
-             })
-
-            if(!memberAlreadyExist){
-                setTeamMembers(newTeamMembers)
-                setPayloadTeam(
-                    [...payloadTeam, {
-                    user_id: Number(id),
-                    workload: Number(hoursMonth)
-                    }]
-                )
-            }
-        }
-
-
-    const redButtonClickHandler = async () => {
-        if(!isEditing) {
-            const newTeamMembers = teamMembers.filter((member) => member.user_id !== professionalClicked)
-            setTeamMembers(newTeamMembers)
-        }
-
-
-        if(isEditing) {
-            await api({
-                method:'delete',     
-                url:`/userProjects/project/${projectId}`,
-                data: {
-                    user_id: professionalClicked,
-                }
-            })
-
-            const {data} = await api({
-                method:'get',     
-                url:`/userProjects/project/${projectId}`,
-            })
-
-
-            setTeamMembers(data)
-        }
-
-        CloseButtonClickHandler()
+    function resetInputs() {
+        setProfessionalSelected(null)
+        setHoursMonth('')
+        setOvertime('')
     }
 
-    ////////////////////// Funções usadas na edição em tempo real //////////////////////
-
-    const addTeamMember = async () => {
-        
-        const projectUsers = await api({
-            method:'post',     
-            url:`/userProjects/project/${projectId}`,
-            data: {
-                user_id: professionalSelected,
-                workload: hoursMonth,
-                extra_hours_limit: overtime
-            }   
-        })
-
-        const {data} = await api({
-            method:'get',     
-            url:`/userProjects/project/${projectId}`,
-        });
-
-        return setTeamMembers(data) && getAllProfessionals()
-        
-    }
-     
-     useEffect(() => {
- 
-         if(componentRendered){
-             setTeamMembers(editData)
-         }
- 
-     }, [componentRendered])
-
-    
-    //////////// Verificações para saber qual das duas edições está acontecendo //////////
-    
-    const professionalClickHandler = (memberId) => {
+    function professionalClickHandler(memberId) {
         setMenuOptionsIsVisible(!menuOptionsIsVisible)
         setProfessionalClicked(memberId)
     }
 
-    const professionalMenuDelete = () => {
-        setTeamMemberDeleteId(professionalClicked)
+    function handleEditModal(hoursMonth, overtime) {
+        setOpenModalEdit(true)
+        setMenuOptionsIsVisible(false)
+        setHoursMonthEdit(hoursMonth)
+        setOvertimeEdit(overtime)
+    }
+
+    function handleRemoveModal() {
         setModalIsVisible(true)
         setMenuOptionsIsVisible(false)
     }
 
-    const editHandler = () => {
-        setOpenModalEdit(true)
-        setMenuOptionsIsVisible(false)
-    }
-
-    const editHoursWhenEditing = async () => {
-        
-        await api({
-            method:'put',     
-            url: `/userProjects/project/${projectId}`,
-            data: {
-                user_id: professionalClicked,
-	            workload: hoursMonthEdit,
-                extra_hours_limit: overtimeEdit
-            }
-        })
-
-        const {data} = await api({
-            method:'get',     
-            url:`/userProjects/project/${projectId}`,
-        });
-
-       
-        setTeamMembers(data);
-        setOpenModalEdit(false)
-    }
-
-    const editHoursWhenRegistering = () => {
-
-        const editedTeamMembers = teamMembers.map((professional) => {
-            if(professional.id === professionalClicked){
-                return {...professional, workload: hoursMonthEdit, extra_hour_limit: overtimeEdit}
-            }
-            return professional
-        })
-
-       
-        setTeamMembers(editedTeamMembers)
-        setOpenModalEdit(false)
-
-    }
-
-    useEffect(() => {
-
-        setHoursMonth('')
-        setOvertime('')
-        setReset(true)
-
-        const fetchData = async () => {
-            await getAllProfessionals()
-        }
-        
-        return fetchData()
-          
-    }, [teamMembers])
-
-
-    useEffect(() => {
-        
-        if(professionalSelected !== null){
-            setReset(false)
-        }
-        
-    }, [professionalSelected])
-
-    /////////////////////////ver isso
-
-    useEffect(() => {
-        const teamArr = teamMembers.filter(professional => professional.id === professionalClicked)
-        const [team] = teamArr
-        setHoursMonthEdit(team?.workload)
-        setOvertimeEdit(team?.extra_hour_limit)
-
-    }, [openModalEdit])
-
     return (
         <AttachmentContainer>
             <SecondaryText margin="0 0 2.5em 0">Time</SecondaryText>
-
             <AttachmentForm>
                 <InputSelectWithLabel
-                    setSelectedOption={setProfessionalSelected}
-                    options={allProfessionals}
+                    setSelectedOption={e => setProfessionalSelected(e.target.value)}
+                    options={options}
                     placeholder="Time"
                     width="100%"
                     lineWidth="40%"
@@ -283,7 +179,7 @@ const AttachmentTeam = ({projectId, componentRendered, editData, payloadTeam, se
                     width="100%"
                     widthLine="20%"
                     placeholder="Horas/mês"
-                    setTextValue={setHoursMonth}
+                    onChange={e => setHoursMonth(e.target.value)}
                     value={hoursMonth}
                     type="number"
                     margin="0 2em 0 2em"
@@ -292,32 +188,32 @@ const AttachmentTeam = ({projectId, componentRendered, editData, payloadTeam, se
                     width="100%"
                     widthLine="20%"
                     placeholder="Horas extras"
-                    setTextValue={setOvertime}
+                    onChange={e => setOvertime(e.target.value)}
                     value={overtime}
                     type="number"
                     margin="0 2em 0 0"
                 />
-                <BlueButton width="13%" onClick={() => isEditing ? addTeamMember() : setTeamMemberClickHandler()}>
+                <BlueButton width="13%" onClick={handleAddMember} type="button">
                     Vincular
                 </BlueButton>
             </AttachmentForm>
-
             <ListHeaderContainer>
-                <ListHeaderTitle width="21.8%" margin="3em" >
+                <ListHeaderTitle width="27%" margin="3em" >
                     Profissional
                 </ListHeaderTitle>
-                <ListHeaderTitle width="21.8%" margin="0">
+                <ListHeaderTitle width="21%" margin="0">
                     Cargo
                 </ListHeaderTitle>
-                <ListHeaderTitle width="21.8%" margin="0">
+                <ListHeaderTitle width="20%" margin="0">
                     Horas mensais
                 </ListHeaderTitle>
-                <ListHeaderTitle width="21.8%" margin="0">
+                <ListHeaderTitle width="23%" margin="0">
                     Horas Extras
                 </ListHeaderTitle>
+                <ListHeaderTitle width="10%" margin="0">
+                </ListHeaderTitle>
             </ListHeaderContainer>
-
-            {teamMembers.map((member, index) => (
+            { rows.map((member, index) => (
                 <AttachmentTableLine key={index}>
                     <ProfessionalInfo>
                         <ProfessionalProfilePicture>
@@ -328,55 +224,57 @@ const AttachmentTeam = ({projectId, componentRendered, editData, payloadTeam, se
                         </ProfessionalName>
                     </ProfessionalInfo>
                     <ProfessionalJob>
-                        {member?.job?.name || member?.job_name}
+                        {member?.job}
                     </ProfessionalJob>
                     <ProfessionalHours>
-                        {member.workload}
+                        {member?.workload}
                     </ProfessionalHours>
-                    <ProfessionalHours>
-                        {member.extra_hour_limit}
-                    </ProfessionalHours>
-                    {modalIsVisible && <ModalRed
-                     id={teamMemberDeleteId}
-                     redButtonClickHandler={redButtonClickHandler}
-                     CloseButtonClickHandler={CloseButtonClickHandler}
-                     title="Excluir profissional"
-                     message="Tem certeza que deseja excluir profissional?"
-                     />}
+                    <ProfessionalOvertime>
+                        {member?.extra_hours_limit || 0}
+                    </ProfessionalOvertime>
+                    <ProfessionalStatus>
+                        <Badge status={member?.status === 1 ? status.ATIVO : status.INATIVO} /> 
+                    </ProfessionalStatus>
+                    { modalIsVisible && 
+                        <ModalRed
+                            id={professionalClicked}
+                            redButtonClickHandler={handleRemoveMember}
+                            CloseButtonClickHandler={() => setModalIsVisible(false)}
+                            title="Remover profissional"
+                            message="Tem certeza que deseja remover profissional?"
+                        />
+                    }
                     <ContainerIcon>
                         <BsThreeDotsVertical 
-                        color="#919EAB"
-                        onClick={() => professionalClickHandler(member.id)}
+                            color="#919EAB"
+                            onClick={() => professionalClickHandler(member.id)}
                         />
                     </ContainerIcon>
-                    {menuOptionsIsVisible && member.id === professionalClicked &&
+                    { menuOptionsIsVisible && member?.id === professionalClicked &&
                         <MenuOptions
-                        positionMenu="25px"
-                        firstOptionDescription="Editar"
-                        secondOptionDescription="Excluir"
-                        firstChosenOption={editHandler}
-                        secondChosenOption={professionalMenuDelete}
-                        padding="0.3em 0.5em 0.3em 1.7em"
-                        id={member.id}
+                            positionMenu="25px"
+                            firstOptionDescription="Editar"
+                            secondOptionDescription="Remover"
+                            firstChosenOption={() => handleEditModal(member?.workload, member?.extra_hours_limit)}
+                            secondChosenOption={handleRemoveModal}
+                            padding="0.3em 0.5em 0.3em 1.7em"
+                            id={member?.id}
                         />
                     }
-
-                    {openModalEdit && <ModalEditAttachment
-                    CloseButtonClickHandler={() => {
-                        setOpenModalEdit(false)
-                    }}
-                    setWorkload={setHoursMonthEdit}
-                    workload={hoursMonthEdit}
-                    setOvertime={setOvertimeEdit}
-                    overtime={overtimeEdit}
-                    saveHandler={isEditing ? editHoursWhenEditing : editHoursWhenRegistering}
-                    />
+                    { openModalEdit && 
+                        <ModalEditAttachment
+                            CloseButtonClickHandler={() => setOpenModalEdit(false)}
+                            setWorkload={setHoursMonthEdit}
+                            workload={hoursMonthEdit}
+                            setOvertime={setOvertimeEdit}
+                            overtime={overtimeEdit}
+                            saveHandler={handleEditMember}
+                        />
                     }
-                </AttachmentTableLine>
+                </AttachmentTableLine> 
             ))}
-
         </AttachmentContainer>
     )
 }
 
-export default AttachmentTeam;
+export default AttachmentTeam
