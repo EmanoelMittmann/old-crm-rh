043932deb726f26f68d6cb242465e9cac6b19ae3
Luis Gustavo Rosa Cardoso <44246590+LuisGustavoRosa@users.noreply.github.com>
2022-07-22 15:46:45 -0300

    make it better

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { LocalStorageKeys } from "../settings/LocalStorageKeys"
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:3333',
+    baseURL: 'https://ubistart-rh-backend.herokuapp.com',
 })
 
 api.interceptors.request.use((config) => {
