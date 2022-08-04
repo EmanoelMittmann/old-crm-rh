91dda9a9d6dd52655d04e870c4f24eeb0efbd0f9
Lucas Bryan <lucasbryan0217@gmail.com>
2022-08-04 14:26:46 -0300

    fix: create authO2 with google

--- src/api/api.js
+++ src/api/api.js
@@ -1,31 +1,33 @@
-import axios from "axios"
-import { LocalStorageKeys } from "../settings/LocalStorageKeys"
+import axios from 'axios';
+import { LocalStorageKeys } from '../settings/LocalStorageKeys';
 
 const api = axios.create({
-    baseURL: 'https://ubistart-rh-backend.herokuapp.com',
-})
+  baseURL: process.env.REACT_APP_URL_API,
+});
 
-api.interceptors.request.use((config) => {
-    const token = JSON.parse(localStorage.getItem(LocalStorageKeys.TOKEN))
+api.interceptors.request.use(
+  (config) => {
+    const token = JSON.parse(localStorage.getItem(LocalStorageKeys.TOKEN));
     const auth = token ? `Bearer ${token}` : '';
-    config.headers.common['Authorization'] = auth
-    return config
-}, (error) => console.error(error)
-    //@TODO - Fazer redirecionamento correto
-)
+    config.headers.common['Authorization'] = auth;
+    return config;
+  },
+  (error) => console.error(error)
+  //@TODO - Fazer redirecionamento correto
+);
 
 api.interceptors.response.use(
-    function (response) {
-      return response
-    },
-    function (error) {
-      if (error?.response?.status === 401 || error?.response?.status === 403) {
-        localStorage.removeItem(LocalStorageKeys.TOKEN)
-        localStorage.removeItem(LocalStorageKeys.USER)
-        window.location.href = '/'
-      }
-      return Promise.reject(error)
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error?.response?.status === 401 || error?.response?.status === 403) {
+      localStorage.removeItem(LocalStorageKeys.TOKEN);
+      localStorage.removeItem(LocalStorageKeys.USER);
+      window.location.href = '/';
     }
-)
+    return Promise.reject(error);
+  }
+);
 
-export default api
+export default api;
--- src/components/atoms/InputSelectCountry/Index.js
+++ src/components/atoms/InputSelectCountry/Index.js
@@ -1,55 +1,55 @@
-
-import React, { useState, useEffect } from 'react'
-import { Country }  from 'country-state-city';
+import React, { useState, useEffect } from 'react';
+import { Country } from 'country-state-city';
 
 import {
-    InputSelectContainer,
-    InputSelectOption,
-    InputSelectOptionPlaceholder,
-    Img
-}from './style'
-import { InputLine } from '../DefaultInput/style'
-import arrowPointingDown from '../../../assets/icons/arrowPointingDown.svg'
+  InputSelectContainer,
+  InputSelectOption,
+  InputSelectOptionPlaceholder,
+  Img,
+} from './style';
+import { InputLine } from '../DefaultInput/style';
+import arrowPointingDown from '../../../assets/icons/arrowPointingDown.svg';
 
-const Index = ({ margin, width, onChange, placeHolder, options, lineWidth, value }) => {
-    const [countries, setCountries] = useState([])
+const Index = ({
+  margin,
+  width,
+  onChange,
+  placeHolder,
+  options,
+  lineWidth,
+  value,
+}) => {
+  const [countries, setCountries] = useState([]);
 
-    useEffect(() => {
-        setCountries(Country.getAllCountries())
-    },[])
-    
- 
-    const attributeValue = {
-        ...(value && { value: value })
-    }
+  useEffect(() => {
+    setCountries(Country.getAllCountries());
+  }, []);
 
-    return (
-        <>
-            
-            <InputLine width={lineWidth} margin={margin}>
-                <InputSelectContainer
-                    {...attributeValue}
-                    width={width}
-                    onChange={onChange}
-                >
-                    <InputSelectOptionPlaceholder disabled selected >
-                        {placeHolder}
-                    </InputSelectOptionPlaceholder>
-                    {countries?.map((option) => (
-                        <InputSelectOption
-                            key={option.isoCode}
-                        >
-                            {option.name}
+  const attributeValue = {
+    ...(value && { value: value }),
+  };
 
-                        </InputSelectOption>
-                    ))}
+  return (
+    <>
+      <InputLine width={lineWidth} margin={margin}>
+        <InputSelectContainer
+          {...attributeValue}
+          width={width}
+          onChange={onChange}
+        >
+          <InputSelectOptionPlaceholder disabled selected>
+            {placeHolder}
+          </InputSelectOptionPlaceholder>
+          {countries?.map((option) => (
+            <InputSelectOption key={option.isoCode}>
+              {option.name}
+            </InputSelectOption>
+          ))}
+        </InputSelectContainer>
+        <Img src={arrowPointingDown} alt="Lupa" />
+      </InputLine>
+    </>
+  );
+};
 
-                   
-                </InputSelectContainer>
-                <Img src={arrowPointingDown} alt="Lupa" />
-            </InputLine>
-        </>
-    )
-}
-
-export default Index
+export default Index;
