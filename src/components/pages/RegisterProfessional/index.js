e097a5fff8d5d1c47d99be2b55827f0bf81a7c8d
EmanoelMittmann <emanoelmittmann@hotmail.com>
2022-09-08 14:55:06 -0300

    feat: arrumado a listagem de jobs

--- src/components/pages/RegisterProfessional/index.js
+++ src/components/pages/RegisterProfessional/index.js
@@ -302,7 +302,7 @@ const RegisterProfessional = () => {
   const optionsJob = useCallback(async () => {
     const response = await api({
       method: "get",
-      url: `/job`,
+      url: `/job/?is_active=1&limit=undefined&?orderField=name&order=asc`,
     });
     setJobs(response.data.data);
   }, []);
