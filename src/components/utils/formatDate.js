0a7d8c9a8caa335c5dd75155fdb378bbd51ef9ff
Emanoel Mittmann <82189310+EmanoelMittmann@users.noreply.github.com>
2022-11-21 09:40:32 -0300

    Update formatDate.js

--- src/components/utils/formatDate.js
+++ src/components/utils/formatDate.js
@@ -1,5 +1,4 @@
 export const formatDate = (dateWithoutFormatting) => {
-    console.log("dateWithoutFormatting: ", dateWithoutFormatting);
     const date = new Date(dateWithoutFormatting)
     const newDate = new Intl.DateTimeFormat('pt-BR').format(date)
     return newDate;
