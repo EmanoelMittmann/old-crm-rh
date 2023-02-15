caba4d5fdfd08755d7acbac12992670081df0399
Cicera Ribeiro <maccicera@gmail.com>
2023-02-15 08:53:08 -0300

    ajuste em formatDate

--- src/components/utils/formatDate.js
+++ src/components/utils/formatDate.js
@@ -1,5 +1,5 @@
 export const formatDate = (dateWithoutFormatting) => {
-    if (!dateWithoutFormatting) return "dd/mm/aaaa"
+    if (!dateWithoutFormatting) return ""
     const date = new Date(dateWithoutFormatting)
     const newDate = new Intl.DateTimeFormat('pt-BR').format(date)
     return newDate; 
