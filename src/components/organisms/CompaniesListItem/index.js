3664ec48ee8076bbdf1de0e4129908743a7b7cdf
Cicera Ribeiro <maccicera@gmail.com>
2023-04-11 08:27:56 -0300

    correção das corres em listagem de empresas

--- src/components/organisms/CompaniesListItem/index.js
+++ src/components/organisms/CompaniesListItem/index.js
@@ -31,19 +31,18 @@ const CompaniesListItem = ({ corporation }) => {
   const buttonRef = useRef(null);
   const dispatch = useDispatch();
 
-
   const tranformStatusCompany =
     corporation.registration_status === 'ACTIVE' ? 'Ativa' :
       corporation.registration_status === 'SUSPENDED' ? 'Suspensa' :
         corporation.registration_status === 'NULL' ? 'Nula' :
           corporation.registration_status === 'UNFIT' ? 'Inapta' : 'Baixada'
   const colorBg =
-    corporation.registration_status === 'ACTIVE' ? '#ddf7e5' :
+    corporation.registration_status === 'ACTIVE' ? '#E4F8DD' : 
       corporation.registration_status === 'SUSPENDED' ? '#fff3d9' :
         corporation.registration_status === 'NULL' ? '#F5F5F5' :
           corporation.registration_status === 'UNFIT' ? '#FFE1E3' : '#FFE1E3'
   const colorText =
-    corporation.registration_status === 'ACTIVE' ? '#1ECB4F' :
+    corporation.registration_status === 'ACTIVE' ? '#229A16' :
       corporation.registration_status === 'SUSPENDED' ? '#FFAE00' :
         corporation.registration_status === 'NULL' ? '#000000' :
           corporation.registration_status === 'UNFIT' ? '#FF3541' : '#FF3541'
